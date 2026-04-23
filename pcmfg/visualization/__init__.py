ed865f56ad775f835e39156616891b091d7b3dd5
Thepbordin <thepbordinjaiinsom@gmail.com>
2026-04-24 01:02:02 +0700

    feat(03-01): export NarrativeOverlayPlotter from visualization __init__
    - Add NarrativeOverlayPlotter to __all__ exports
    - Enables from pcmfg.visualization import NarrativeOverlayPlotter

--- pcmfg/visualization/__init__.py
+++ pcmfg/visualization/__init__.py
@@ -1,5 +1,6 @@
 """Visualization module for PCMFG."""
 
+from pcmfg.visualization.overlay_plotter import NarrativeOverlayPlotter
 from pcmfg.visualization.plotter import EmotionPlotter
 
-__all__ = ["EmotionPlotter"]
+__all__ = ["EmotionPlotter", "NarrativeOverlayPlotter"]
